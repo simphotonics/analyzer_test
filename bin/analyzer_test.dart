ccaec6cbaa8b91feeb72af26a20c3ec4ec6af7b4
simphotonics <git@simphotonics.com>
2020-08-06 18:50:15 +0100

    first commit

---
+++ bin/analyzer_test.dart
@@ -0,0 +1,5 @@
+import 'package:analyzer_test/analyzer_test.dart' as analyzer_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${analyzer_test.calculate()}!');
+}
---
+++ test/analyzer_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:analyzer_test/analyzer_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
