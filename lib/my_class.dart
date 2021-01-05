467aaa4e00b79d147384f00216dde4c2adc4a0ee
simphotonics <git@simphotonics.com>
2021-01-05 19:28:10 +0000

    Initial commit.

---
+++ lib/my_class.dart
@@ -0,0 +1,14 @@
+class MyClass {
+  MyClass(int a) {
+    a = this.a;
+    _b = a;
+  }
+
+  late final int _b;
+  late final int a;
+
+  @override
+  String toString() {
+    return _b.toString();
+  }
+}
