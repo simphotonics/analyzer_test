7cbc9389bb6a5f8e36765dad50cf0d8f200ae3f9
simphotonics <git@simphotonics.com>
2021-01-05 19:05:03 +0000

    Removed test folder. Increased min. Dart SDK v.

--- lib/analyzer_test.dart
+++ lib/analyzer_test.dart
@@ -1,3 +1,18 @@
 int? calculate() {
   return 6 * 7;
 }
+
+class MyClass {
+  MyClass(int a) {
+    a = this.a;
+    _b = a;
+  }
+
+  late final int _b;
+  late final int a;
+
+  @override
+  String toString() {
+  return _b.toString();
+   }
+}
--- test/analyzer_test_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:analyzer_test/analyzer_test.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
